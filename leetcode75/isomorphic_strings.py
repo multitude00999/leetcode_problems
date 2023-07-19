cf3327d44a7dc1a6cde70c92d0ebd41cd5e75765
Surya Pratap Singh Parmar <surya@suryas-mbp.ads.northwestern.edu>
2023-07-19 17:35:01 -0500

    isomorphic strings optimized solution

--- leetcode75/isomorphic_strings.py
+++ leetcode75/isomorphic_strings.py
@@ -3,32 +3,17 @@ class Solution:
         n, m = len(s), len(t)
         if n != m:
             return False
-        countCharS = {}
-        countCharT = {}
+        mapS = {}
+        mapT = {}
 
         for i in range(n):
-            if s[i] in countCharS:
-                countCharS[s[i]] += 1 
-            else:
-                countCharS[s[i]] = 1 
-            
-            if t[i] in countCharT:
-                countCharT[t[i]] += 1 
+            if s[i] in mapS:
+                if mapS[s[i]] != t[i]:
+                    return False
 
             else:
-                countCharT[t[i]] = 1
-
-        s_sh = ""
-        t_sh = ""
-        for i in range(n):
-            if countCharS[s[i]] !=  countCharT[t[i]]:
-                return False
-            if len(s_sh)==0 or s_sh[-1]!=s[i] :
-                s_sh+=s[i]
-            if len(t_sh)==0 or t_sh[-1]!=t[i]:
-                t_sh+=t[i]
-        if len(s_sh)!=len(t_sh):
-            return False
+                if t[i] in mapT:
+                    return False
+                mapS[s[i]] = t[i]
+                mapT[t[i]] = s[i]
         return True
-
-        
