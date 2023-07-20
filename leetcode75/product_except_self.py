eec8316f195ab96541e096ba2d19a884fd748981
multitude00999 <suryaparmar2024@u.northwestern.edu>
2023-07-19 19:20:17 -0500

    product except self improved

--- leetcode75/product_except_self.py
+++ leetcode75/product_except_self.py
@@ -1,28 +1,13 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prod = 1
-        cnt_z = 0
-        n = len(nums)
+        pref, suff, n = 1, 1, len(nums)
+        res = [1]*n
 
-        # calculate product of array ignoring zeroes
+        # using prefix and suffix 
         for i in range(n):
-            if nums[i]==0:
-                cnt_z+=1
-            else:
-                prod *= nums[i]
-        
-        # do inplace replacement for prod in the array
-        for i in range(n):
-            if cnt_z>1:
-                nums[i]=0
-                continue
-            elif cnt_z == 1:
-                if nums[i]!=0:
-                    nums[i]= 0
-                else:
-                    nums[i]=prod
+            res[i] *= pref
+            pref *= nums[i]
+            res[-1-i] *= suff
+            suff*= nums[-1-i] 
 
-            else:
-                nums[i] = prod//nums[i]
-
-        return nums
+        return res
