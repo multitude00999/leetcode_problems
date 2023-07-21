4c66e2ca772735797ad0c8313c89b98e64e2d409
multitude00999 <suryaparmar2024@u.northwestern.edu>
2023-07-20 21:45:19 -0500

    move zeroes improved

--- leetcode75/moveZeroes.py
+++ leetcode75/moveZeroes.py
@@ -6,21 +6,12 @@ class Solution:
         n = len(nums)
         if n<2:
             return
-        ptr1, ptr2 = 0, 1
-        while ptr1 < n and ptr2 < n:
-            # iterate until a zero element is found
-            while ptr1 < n and nums[ptr1]!=0:
-                ptr1+=1
-
-            # find the closest non zero element 
-            ptr2 = ptr1 + 1
-            while ptr2 < n and nums[ptr2]==0:
-                ptr2+=1
+        ptr1 = 0
+        for ptr2 in range(n):
+            # checkif there is non zero number and zero number to be swapped
+            if nums[ptr2]!=0 and nums[ptr1] == 0:
+                nums[ptr1], nums[ptr2] = nums[ptr2], nums[ptr1]
             
-            # check break condition
-            if ptr1 > n-1 or ptr2 > n-1:
-                return
-            # swap the non zero and zero value
-            nums[ptr1], nums[ptr2] = nums[ptr2], nums[ptr1]
-            ptr1+=1
-            ptr2+=1
+            # check for zero value for pointer 1
+            if nums[ptr1]!=0:
+                ptr1+=1
